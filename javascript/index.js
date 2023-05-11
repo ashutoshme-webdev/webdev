23806ed4a504ee3d22a496493e93dc85fde04524
ashutoshme-webdev <131554484+ashutoshme-webdev@users.noreply.github.com>
2023-05-11 23:57:16 +0530

    javascript basic

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,77 @@
 console.log("linking .js file");
+// let Swe=4645;
+// console.log(Swe); checking for non-camelcase
 
-let a = true;
-console.log(a);
+
+// let a = true;
+// console.log(a);
+
+// const num = 12;
+// //num = 13;
+
+// let lastName = 3;
+// console.log(lastName);
+
+// lastName = true;
+// console.log(lastName);
+
+// var as=7;
+
+// console.log(as)
+// var as=false;
+// console.log(as);
+// let obj={
+//     firstName: 'Ash',
+//     lastName: 'Ketchup'
+// };
+// console.log(obj.firstName)
+// console.log(obj['lastName'])
+
+// let nums=[5,8,9,3];
+// console.log(nums[2]);
+// nums[4]=75;
+// nums[1]=88;
+// console.log(nums)
+
+
+// console.log(5 !== 3);
+
+// let age = 27;
+// let status = (age >= 18) ? 'Yes Vote' : 'No Vote';
+
+// console.log(status);
+
+// let marks = 12;
+
+// if(marks >= 90) {
+//     console.log('A');
+// }
+// else if(marks >= 80) {
+//     console.log('B');
+// }
+// else if(marks >=70) {
+//     console.log('C');
+// }
+// else if(marks >= 60) {
+//     console.log('D');
+// }
+// else {
+//     console.log('E');
+//}
+
+// let num = 1;
+
+// switch(num) {
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     default: console.log('D');
+// }
+
+
+
+
+
