92f6cd656dec02769ae5fae058db1a71d0776c2c
ashutoshme-webdev <ashutosh.asme@gmail.com>
2023-06-10 00:49:57 +0530

    studynotion app

---
+++ react/studynotion/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = ({isLoggedIn}) => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full'>
+      You are inside Homepage<br/><br/>
+      Please click Login/Signup 
+    </div>
+  )
+}
+
+export default Home
